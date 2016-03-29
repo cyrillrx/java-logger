16cd7e4493316fcc958ee81d19d351fdbcf9a493
Cyril Leroux <cyril0leroux@gmail.com>
2016-03-29 20:59:20 +0200

    2016-03-29 - v1.1.2 - Extended Severity class visibility

--- Logger/src/main/java/com/cyrillrx/logger/Severity.java
+++ Logger/src/main/java/com/cyrillrx/logger/Severity.java
@@ -4,7 +4,7 @@ package com.cyrillrx.logger;
  * @author Cyril Leroux
  *         Created on 20/10/15
  */
-class Severity {
+public class Severity {
 
     public static final int FATAL   = 0;
     public static final int ERROR   = 1;
